d7804bf4ad8c526e082acff4a217704ad60f7b47
user-0021 <nabeyamaoden@gmail.com>
2025-02-17 11:51:11 +0900

    add comment

--- linear_list.h
+++ linear_list.h
@@ -14,14 +14,73 @@ void do_not_use_list_release(void* list,unsigned int size);
 void do_not_use_list_push(void* list,const void* const data,unsigned int size);
 void do_not_use_list_deleate(void* itr,unsigned int size);
 
+/*
+ *  LINEAR_LIST_NEXT(list)
+ * 
+ *  listの次の要素のイテレーターを返します、型はリストと同様です。
+ *　次の要素がない場合返り値はNULLです
+*/
 #define LINEAR_LIST_NEXT(list) ((typeof(list))(*(void**)((void*)list + sizeof(typeof(*list)))))
+
+/*
+ *  LINEAR_LIST_PREV(list)
+ * 
+ *  listの前の要素のイテレーターを返します、型はリストと同様です。
+ *  前の要素がない場合返り値はNULLです
+*/
 #define LINEAR_LIST_PREV(list) ((typeof(list))(*(void**)((void*)list +sizeof(typeof(*list))+sizeof(void*))))
 
+
+/*
+ *  LINEAR_LIST_CREATE(type)
+ * 
+ *  typeで指定された型の空のリストを返します。
+ *  返されるリストは type*型です
+*/
 #define LINEAR_LIST_CREATE(type) ((type*)do_not_use_list_create(sizeof(type),(void*)0,(void*)0))
+
+/*
+ *  LINEAR_LIST_RELEASE(list)
+ * 
+ *  listを開放します
+*/
 #define LINEAR_LIST_RELEASE(list) do_not_use_list_release(list,sizeof(typeof(*list)))
+
+/*
+ *  LINEAR_LIST_PUSH(list,data)
+ * 
+ *  listにdataをPUSHします。
+ *　dataはLINEAR_LIST_CREATEで指定した方と同じ必要があります
+*/
 #define LINEAR_LIST_PUSH(list,data) do_not_use_list_push(list,&data,sizeof(typeof(*list)))
+
+/*
+ *  LINEAR_LIST_ERASE(itr)
+ * 
+ *  listからitrの要素を削除します。
+*/
 #define LINEAR_LIST_ERASE(itr) do_not_use_list_deleate(itr,sizeof(typeof(*itr)));
 
+
+/*
+ *  LINEAR_LIST_LAST(list)
+ * 
+ *  listの最後の要素を取得します。
+ *　型はlistと同様です
+*/
 #define LINEAR_LIST_LAST(list) ((typeof(list))do_not_use_list_get_last(list,sizeof(typeof(*list))))
+
+/*
+ *  LINEAR_LIST_FOREACH(list,iter)
+ * 
+ *  listの全ての要素をiterに入れて次のコードブロックを実行します。
+ *　for文のヘッダと同じです。
+*/
 #define LINEAR_LIST_FOREACH(list,iter) for(iter = *(void**)(((void*)list) + sizeof(*list));iter != NULL;iter = LINEAR_LIST_NEXT(iter))
+
+/*
+ *  LINEAR_LIST_FOREACH_R(list,iter)
+ * 
+ *  LINEAR_LIST_FOREACHと同様ですが、代入は後ろから始まります。
+*/
 #define LINEAR_LIST_FOREACH_R(list,iter) for(iter = LINEAR_LIST_LAST(list);iter != list;iter = LINEAR_LIST_PREV(iter))
